8b0f4770a0fafa22eb1d4dee8639a368bf1d3482
Aakash <aakashanand1995@gmail.com>
2024-04-04 15:37:59 +0530

    verifyemail completed

--- src/app/api/users/verifyEmail/route.ts
+++ src/app/api/users/verifyEmail/route.ts
@@ -1,10 +1,34 @@
+import { User } from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  console.log("hello");
-  const bodyData = await request.json();
-  console.log(bodyData);
-  NextResponse.json(bodyData);
+  const reqBody = await request.json();
+  const { token } = reqBody;
+
+  if (token !== null) {
+    const userFromDB = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (userFromDB) {
+      userFromDB.isVerified = true;
+      userFromDB.verifyToken = undefined;
+      userFromDB.verifyTokenExpiry = undefined;
+
+      await userFromDB.save();
+
+      return NextResponse.json({
+        message: "Email verified Successfully",
+        status: 200,
+        textStatus: true,
+      });
+    } else {
+      return NextResponse.json({ message: "User Not Found", status: 400 });
+    }
+  } else {
+    return NextResponse.json({ message: "invalid URL", status: 400 });
+  }
 }
 
 export async function GET(request: NextRequest) {
